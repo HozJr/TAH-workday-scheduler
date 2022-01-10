04d21c60acd1c069a2b4e036fc6a32e25305299f
HozJr <hozjr2006@gmail.com>
2022-01-09 22:33:30 -0600

    local storage

--- script.js
+++ script.js
@@ -170,3 +170,19 @@ function testTime() {
     };
 }
 testTime();
+// Loops through input area to get item from local storage
+var x = [9, 10, 11, 12, 1, 2, 3, 4, 5];
+// Test loop:
+for (var i = 0; i < x.length; i++) {
+    var dataHour = localStorage.getItem(x[i]);
+    // form - control
+    $(".form" + x[i]).val(dataHour);
+}
+// Event listener to save to local stroage
+$(".saveBtn").click(function () {
+    event.preventDefault();
+    var formValue = $(this).siblings(".form-control").val();
+    var listItem = $(this).parent().data("hour");
+
+    localStorage.setItem(listItem, formValue);
+});
